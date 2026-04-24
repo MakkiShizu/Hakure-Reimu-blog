54236f8c27bb4f4c2963a64856f949ec908cd272
MakkiShizu <MakkiShizu@users.noreply.github.com>
2026-04-24 07:53:13 +0000

    deploy: 18429c5dbe8184d4aee8a20fd4b2d2caf80815a1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1775042845072";const preCache = ["/Hakure-Reimu-blog/images/taichi.png","/Hakure-Reimu-blog/images/banner.webp","/Hakure-Reimu-blog/css/loader.css","/Hakure-Reimu-blog/css/style.css","/Hakure-Reimu-blog/js/script.js"];const cacheDomain = [
+const VERSION = "1777017184558";const preCache = ["/Hakure-Reimu-blog/images/taichi.png","/Hakure-Reimu-blog/images/banner.webp","/Hakure-Reimu-blog/css/loader.css","/Hakure-Reimu-blog/css/style.css","/Hakure-Reimu-blog/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
